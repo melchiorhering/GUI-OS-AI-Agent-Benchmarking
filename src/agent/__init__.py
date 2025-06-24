b798848e51e2bb43f6a3f45378c225264e833273
Stijn Hering <stijn@hering.nl>
2025-06-24 14:02:21 +0200

    feat: working

--- src/agent/__init__.py
+++ src/agent/__init__.py
@@ -2,3 +2,14 @@ from .executor import SandboxExecutor
 from .logger import CustomColoredFormatter, LogColors, SandboxAgentLogger, get_orchestrator_logger
 from .sandbox_agent import SandboxCodeAgent
 from .tools.callbacks import initial_state_callback, observation_screenshot_callback
+
+__all__ = [
+    "SandboxExecutor",
+    "CustomColoredFormatter",
+    "LogColors",
+    "SandboxAgentLogger",
+    "get_orchestrator_logger",
+    "SandboxCodeAgent",
+    "initial_state_callback",
+    "observation_screenshot_callback",
+]
